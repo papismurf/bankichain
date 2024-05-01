3b057db6213a74767897159dbdbb2a0a0ae76ea5
dbpython <33459750+papismurf@users.noreply.github.com>
2024-05-01 18:33:24 +0100

    initial commit

--- projects/bankichain-contracts/smart_contracts/banki/contract.py
+++ projects/bankichain-contracts/smart_contracts/banki/contract.py
@@ -1,9 +1,25 @@
-import beaker
+import beaker as bk
 import pyteal as pt
 
-app = beaker.Application("banki")
+
+class ContractState:
+    result = bk.GlobalStateValue(pt.TealType.bytes)
+
+
+app = bk.Application("Banki", state=ContractState())
 
 
 @app.external
 def hello(name: pt.abi.String, *, output: pt.abi.String) -> pt.Expr:
     return output.set(pt.Concat(pt.Bytes("Hello, "), name.get()))
+
+
+@app.external(read_only=True)
+def set_result(*, result: pt.abi.Bytes) -> pt.Expr:
+    return result.set(app.state.result)
+
+
+if __name__ == "__main__":
+    spec = app.build()
+    spec.export("artifacts")
+
